e7692719fbfef5eeecaf66ff62266b0b34ad590d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-16 15:34:47 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mitch Andrade on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
