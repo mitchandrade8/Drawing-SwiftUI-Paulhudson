586b9e06c84edb185b0cf6d8c5031676a521417b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-18 12:35:41 -0600

    second commit

---
+++ Drawing/Practice/AnimatingComplexShapesWithAnimatablePair.swift
@@ -0,0 +1,20 @@
+//
+//  AnimatingComplexShapesWithAnimatablePair.swift
+//  Drawing
+//
+//  Created by Mitch Andrade on 3/18/22.
+//
+
+import SwiftUI
+
+struct AnimatingComplexShapesWithAnimatablePair: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimatingComplexShapesWithAnimatablePair_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingComplexShapesWithAnimatablePair()
+    }
+}
